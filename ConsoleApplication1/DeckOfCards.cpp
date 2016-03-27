c2858a40c3f0118d3caa75ea7f3d0ce8ae5720f2
Nolwazi Ngcobo <214553811@stu.ukzn.ac.za>
2016-03-28 01:11:43 +0200

    shuffle done

--- ConsoleApplication1/DeckOfCards.cpp
+++ ConsoleApplication1/DeckOfCards.cpp
@@ -13,6 +13,7 @@ vector <Card*> DeckOfCards::reset()
 	int CardIndex = 0; //stores the card number to be stored
 	free();
 
+	cards = 20;
 	for (i = 0; i < 20; ++i)
 	{
 		++CardIndex;
@@ -33,6 +34,30 @@ vector <Card*> DeckOfCards::reset()
 }
 
 
+void DeckOfCards::shuffle()
+{
+	if (cards > 2)
+	{
+		int i;
+
+		for (i = 0; i < 50; ++i)
+		{
+			Switch(Cards[rand() % cards + 1], Cards[rand() % 6 + 1]);
+		}
+	}
+
+	else
+		cout << "too little cards to shuffle" << endl;
+
+}
+
+void DeckOfCards::Switch(Card *c1, Card *c2)
+{
+	Card Hold = *c1;
+	*c1 = *c2;
+	*c2 = Hold;
+}
+//frees up memory allocataed to the array of pointers
 void DeckOfCards::free()
 {
 	int i;
--- ConsoleApplication1/DeckOfCards.h
+++ ConsoleApplication1/DeckOfCards.h
@@ -9,11 +9,15 @@ class DeckOfCards
 public:
 	DeckOfCards();
 	vector <Card*> reset();
+	void shuffle();
 	~DeckOfCards();
 
 private:
 	void free();
+	void Switch(Card *, Card *);
+	int cards = 0; // number of cards in the deck
 	vector <Card*> Cards;
+	
 };
 
 #endif
